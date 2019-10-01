ce32a1aaec22b93b37003b071f64f2be8c815f17
Heather <hlmathies@gmail.com>
2019-10-01 08:25:05 -0400

    backend now serving frontend build

---
+++ src/build/precache-manifest.eb3d343739d5e75b8ade120ab9f6b1e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cce192522413578af1b3bda5c45270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd9ac32f52ca30022acb",
+    "url": "/static/css/main.c58b48e1.chunk.css"
+  },
+  {
+    "revision": "72ac27f206306a2501e9",
+    "url": "/static/js/2.261b1987.chunk.js"
+  },
+  {
+    "revision": "cd9ac32f52ca30022acb",
+    "url": "/static/js/main.2d8195fc.chunk.js"
+  },
+  {
+    "revision": "3a1657ec8aeaafb2f67e",
+    "url": "/static/js/runtime-main.82f07dd6.js"
+  }
+]);
