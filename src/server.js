9de423bec542b649fbfa9dfbab964ac7afc252eb
Heather <hlmathies@gmail.com>
2019-10-06 10:19:47 -0400

    add build folder

--- src/server.js
+++ src/server.js
@@ -65,7 +65,7 @@ app.post('/api/articles/:name/add-comment', async (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + './build/index.html'))
+  res.sendFile(path.join(__dirname + '/build/index.html'))
 })
 
 app.listen(8000, () => console.log("listening on port 8000"));
